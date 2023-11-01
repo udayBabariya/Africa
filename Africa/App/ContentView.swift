5cbc5872f02b759f397f6a55402f935afd92ff63
Uday Babariya <uday.babariya.77@gmail.com>
2023-11-01 07:42:31 +0530

    added dynamic grid view as pr buttoon click on the toolbar

--- Africa/App/ContentView.swift
+++ Africa/App/ContentView.swift
@@ -12,9 +12,32 @@ struct ContentView: View {
     //MARK: - Property
     let animals: [Animal] = Bundle.main.decode("animals.json")
     let haptics = UIImpactFeedbackGenerator(style: .medium)
-    let gridLayout: [GridItem] = Array(repeating: GridItem(.flexible(minimum: 0, maximum: .infinity)), count: 2)
     
     @State private var isGridViewActive: Bool = false
+    @State private var gridLayout: [GridItem] = [GridItem(.flexible(minimum: 0, maximum: .infinity))]
+    @State private var gridColumn = 1
+    @State private var toolbarIcon: String = "square.grid.2x2"
+    
+    // MARK: - functions
+    
+    func gridSwitch() {
+        gridLayout = Array(repeating: .init(.flexible(minimum: 0, maximum: .infinity)), count: gridLayout.count % 3 + 1)
+        gridColumn = gridLayout.count
+        print("grid number: \(gridColumn)")
+        
+        //Toolbar Image
+        switch gridColumn {
+        case 1:
+            toolbarIcon = "square.grid.2x2"
+        case 2:
+            toolbarIcon = "square.grid.3x3"
+        case 3:
+            toolbarIcon = "rectangle.grid.1x2"
+        default:
+            toolbarIcon = "square.grid.2x2"
+        }
+    }
+    
     
     var body: some View {
         
@@ -68,8 +91,9 @@ struct ContentView: View {
                             print("grid")
                             isGridViewActive = true
                             haptics.impactOccurred()
+                            gridSwitch()
                         }, label: {
-                            Image(systemName: "square.grid.2x2")
+                            Image(systemName: toolbarIcon)
                                 .font(.title2)
                                 .foregroundColor(isGridViewActive ? .accent : .primary)
                         })
