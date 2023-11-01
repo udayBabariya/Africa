d7ed1e98fd9c785899f62c2320a8f34603fe1ac4
Uday Babariya <uday.babariya.77@gmail.com>
2023-11-01 09:09:03 +0530

    Added About View and Center View Modifire

---
+++ Africa/Modifire/CenterModifire.swift
@@ -0,0 +1,8 @@
+//
+//  CenterModifire.swift
+//  Africa
+//
+//  Created by Uday on 01/11/23.
+//
+
+import Foundation
---
+++ Africa/View/CreditsView.swift
@@ -0,0 +1,18 @@
+//
+//  CreditsView.swift
+//  Africa
+//
+//  Created by Uday on 01/11/23.
+//
+
+import SwiftUI
+
+struct CreditsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CreditsView()
+}
